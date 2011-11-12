bd32ce9c277ecf8bc93c4b3c59e7dc473185bd6c
Peter Simons <simons@cryp.to>
2011-11-12 16:16:50 +0100

    package-list.hs: ignore ghc 7.2.2 packages

--- package-list.hs
+++ package-list.hs
@@ -45,6 +45,9 @@ stripProfilingVersions pkgs = [ p | p@(_,_,attr) <- pkgs , not (attr =~ "ghc[0-9
 stripGhc721Versions :: Pkgset -> Pkgset
 stripGhc721Versions pkgs = [ p | p@(_,_,attr) <- pkgs , not (attr =~ "ghc721") ]
 
+stripGhc722Versions :: Pkgset -> Pkgset
+stripGhc722Versions pkgs = [ p | p@(_,_,attr) <- pkgs , not (attr =~ "ghc722") ]
+
 selectLatestVersions :: Pkgset -> Pkgset
 selectLatestVersions = nubBy (\x y -> comparePkgByName x y == EQ) . sortBy comparePkgByVersion
 
@@ -67,5 +70,5 @@ main :: IO ()
 main = do
   hackage <- readHackage
   pkgset' <- fmap (filter (isHackagePackage hackage)) getHaskellPackageList
-  let pkgset = (selectLatestVersions . stripGhc721Versions . stripProfilingVersions) pkgset'
+  let pkgset = (selectLatestVersions . stripGhc721Versions . stripGhc722Versions . stripProfilingVersions) pkgset'
   mapM_ (putStrLn . formatPackageLine) (sortBy comparePkgByName pkgset)
