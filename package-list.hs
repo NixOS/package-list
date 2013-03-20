ca3e5115bd55a65fd983528bb4a4ca5f3a2d15b8
Peter Simons <simons@cryp.to>
2013-03-20 11:37:59 +0100

    Polished and tested the code.
    The new version uses a slightly more sophisticated algorithm to select the
    preferred attribute path among several alternatives. Given several alternatives by
    "nix-env -qa *", the latest version is generally preferred. If that version has
    more than one attribute path, the one with the shortest initial segment is
    preferred, i.e. "haskellPackages" is more desirable than "haskellPackages_ghc742".
    If the package has more than one entry in of the same attribute set, then the
    shortest path is preferred. If that criteria is still ambiguous, then the
    path that is lexicographically smaller is chosen.
    
    For example, the alternatives
    
      haskellPackages_ghc762.QuickCheck_1_2_0_0  haskell-QuickCheck-ghc7.6.2-1.2.0.0
      haskellPackages_ghc762.QuickCheck1         haskell-QuickCheck-ghc7.6.2-1.2.0.1
      haskellPackages_ghc762.QuickCheck_2_1_1_1  haskell-QuickCheck-ghc7.6.2-2.1.1.1
      haskellPackages_ghc762.QuickCheck_2_4_0_1  haskell-QuickCheck-ghc7.6.2-2.4.0.1
      haskellPackages_ghc762.QuickCheck_2_4_1_1  haskell-QuickCheck-ghc7.6.2-2.4.1.1
      haskellPackages_ghc762.QuickCheck_2_4_2    haskell-QuickCheck-ghc7.6.2-2.4.2
      haskellPackages_ghc762.QuickCheck_2_5_1_1  haskell-QuickCheck-ghc7.6.2-2.5.1.1
      haskellPackages_ghc762.QuickCheck          haskell-QuickCheck-ghc7.6.2-2.6
      haskellPackages.QuickCheck_1_2_0_0         haskell-QuickCheck-ghc7.4.2-1.2.0.0
      haskellPackages.QuickCheck1                haskell-QuickCheck-ghc7.4.2-1.2.0.1
      haskellPackages.QuickCheck_2_1_1_1         haskell-QuickCheck-ghc7.4.2-2.1.1.1
      haskellPackages.QuickCheck_2_4_0_1         haskell-QuickCheck-ghc7.4.2-2.4.0.1
      haskellPackages.QuickCheck_2_4_1_1         haskell-QuickCheck-ghc7.4.2-2.4.1.1
      haskellPackages.QuickCheck_2_4_2           haskell-QuickCheck-ghc7.4.2-2.4.2
      haskellPackages.QuickCheck2                haskell-QuickCheck-ghc7.4.2-2.6
      haskellPackages.QuickCheck                 haskell-QuickCheck-ghc7.4.2-2.5.1.1
    
    are narrowed down to the latest version:
    
      haskellPackages_ghc762.QuickCheck          haskell-QuickCheck-ghc7.6.2-2.6
      haskellPackages.QuickCheck2                haskell-QuickCheck-ghc7.4.2-2.6
    
    Now,
    
      haskellPackages.QuickCheck2                haskell-QuickCheck-ghc7.4.2-2.6
    
    is chosen, because its path has the shortest initial segment.

--- package-list.hs
+++ package-list.hs
@@ -1,36 +1,40 @@
 module Main ( main ) where
 
-import Control.Applicative
-import Data.Char
-import qualified Data.Map as Map
-import Distribution.Hackage.DB ( readHackage, Hackage, PackageIdentifier(..), PackageName(..), Version(..) )
-import Distribution.Text
+import Prelude hiding ( lookup )
+import Distribution.Hackage.DB hiding ( null, foldr, map )
+import Distribution.Text ( Text(..), display, simpleParse )
+import Distribution.Compat.ReadP ( ReadP, (<++), char, munch1, string, look, skipSpaces, pfail )
 import Text.PrettyPrint ( text )
-import Distribution.Compat.ReadP
+import Control.Monad ( unless )
+import Data.Char ( isSpace, toLower )
+import Data.Ord ( comparing )
+import Data.List ( sortBy )
+import Data.Maybe ( isJust )
 import System.Process ( readProcess )
 
 newtype Path = Path String
   deriving (Show, Eq, Ord)
 
-instance Text Path where
-  disp (Path p) = text p
-  parse = Path <$> munch1 (not . isSpace)
-
 data NixPkg = NixPkg Path PackageIdentifier
   deriving (Show, Eq, Ord)
+
+type PkgSet = Map PackageName (Version,Path)
+
+instance Text Path where
+  disp (Path p) = text p
+  parse = Path `fmap` munch1 (not . isSpace)
 
 instance Text NixPkg where
   disp (NixPkg _ pid) = disp pid
   parse = do path <- parse
              _ <- skipSpaces
-             (pname,pver) <- hsLibrary <++ hsExecutable +++ other
+             (pname,pver) <- hsLibrary <++ hsExecutable <++ other
              return (NixPkg path (PackageIdentifier pname pver))
     where
       hsLibrary    = do { string "haskell-"; pname <- parse; char '-'; string "ghc"; parse :: ReadP r Version; char '-'; pver <- parse; return (pname,pver) }
-      hsExecutable = do { pname <- parse; char '-'; pver <- parse; return (pname, pver) }
+      hsExecutable = do { PackageIdentifier pname pver <- parse; pEof; return (pname, pver) }
       other        = do { pname <- munch1 (not . isSpace); return (PackageName pname, Version [] []) }
-
-type PkgSet = Map.Map PackageName (Version,Path)
+      pEof         = look >>= \s -> unless (null s) pfail
 
 readNixPkgList :: IO [NixPkg]
 readNixPkgList = readProcess "sh" ["-c", "nix-env -qaP '*' 2>/dev/tty"] "" >>= mapM p . lines
@@ -39,10 +43,10 @@ readNixPkgList = readProcess "sh" ["-c", "nix-env -qaP '*' 2>/dev/tty"] "" >>= m
     p s = maybe (fail ("cannot parse: " ++ show s)) return (simpleParse s)
 
 makeNixPkgSet :: Hackage -> [NixPkg] -> PkgSet
-makeNixPkgSet db pkgs = foldr (uncurry (Map.insertWith f)) Map.empty [ (pn,(pv,p)) | NixPkg p (PackageIdentifier pn pv) <- pkgs, isOnHackage pn pv ]
+makeNixPkgSet db pkgs = foldr (uncurry (insertWith f)) empty [ (pn,(pv,p)) | NixPkg p (PackageIdentifier pn pv) <- pkgs, isOnHackage pn pv ]
   where
     isOnHackage :: PackageName -> Version -> Bool
-    isOnHackage (PackageName n) v = maybe False (const True) (Map.lookup n db)
+    isOnHackage (PackageName n) v = isJust (lookup n db >>= lookup v)
 
     f :: (Version,Path) -> (Version,Path) -> (Version,Path)
     f x@(v1,p1@(Path path1)) y@(v2,p2@(Path path2))
@@ -54,7 +58,10 @@ makeNixPkgSet db pkgs = foldr (uncurry (Map.insertWith f)) Map.empty [ (pn,(pv,p
       | length path1 > length path2                                         = y
       | p1 > p2                                                             = x
       | p1 < p2                                                             = y
-      | otherwise                                                           = error ("cannot decide ordering of " ++ show x ++ " versus " ++ show y)
+      | otherwise = error ("cannot decide ordering of " ++ show x ++ " versus " ++ show y)
+
+sortCaseless :: [(PackageName, (Version,Path))] -> [(PackageName, (Version,Path))]
+sortCaseless = sortBy (\(PackageName x, _) (PackageName y, _) -> comparing (map toLower) x y)
 
 formatPackageLine :: (PackageName,(Version,Path)) -> String
 formatPackageLine (name, (version, path)) = show (display name, display version, Just url)
@@ -64,5 +71,5 @@ formatPackageLine (name, (version, path)) = show (display name, display version,
 main :: IO ()
 main = do
   hackage <- readHackage
-  pkgset <- makeNixPkgSet hackage <$> readNixPkgList
-  mapM_ (putStrLn . formatPackageLine) (Map.toAscList pkgset)
+  pkgset <- makeNixPkgSet hackage `fmap` readNixPkgList
+  mapM_ (putStrLn . formatPackageLine) (sortCaseless (toList pkgset))
