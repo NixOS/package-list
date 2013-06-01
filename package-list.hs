bc9e4fabe7544dc72d6626303a81de6b5c2d55e2
Peter Simons <simons@cryp.to>
2013-06-01 12:38:50 +0200

    package-list.hs: filter out 'nodePackages' from "nix-env -q \*" so that our parser doesn't have to deal with their freaky attribute names

--- package-list.hs
+++ package-list.hs
@@ -37,7 +37,7 @@ instance Text NixPkg where
       pEof         = look >>= \s -> unless (null s) pfail
 
 readNixPkgList :: IO [NixPkg]
-readNixPkgList = readProcess "sh" ["-c", "nix-env -qaP '*' 2>/dev/tty"] "" >>= mapM p . lines
+readNixPkgList = readProcess "sh" ["-c", "nix-env -qaP '*' 2>/dev/tty", "|", "grep", "-v", "nodePackages"] "" >>= mapM p . lines
   where
     p :: String -> IO NixPkg
     p s = maybe (fail ("cannot parse: " ++ show s)) return (simpleParse s)
